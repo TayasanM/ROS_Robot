b0816e90a477b29e1e17236c4014ec027f5f0955
Tayasan Milinda <malinda19900407@gmail.com>
2026-05-24 16:40:23 +0200

    Add launch file for kcf_follow node

---
+++ src/rosmaster_m1/launch/kcf_follow.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('rosmaster_m1')
+    params = os.path.join(pkg_dir, 'config', 'kcf_params.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='rosmaster_m1',
+            executable='kcf_follow',
+            name='kcf_follow',
+            output='screen',
+            parameters=[params]
+        )
+    ])
